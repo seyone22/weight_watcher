d0870a80433220a8841a700c52ae772b993d3b6f
seyone <s.g.seyone@protonmail.com>
2022-02-08 19:59:42 +0530

    Add project files.

---
+++ MainWindow.xaml.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "MainWindow.g.h"
+
+namespace winrt::weight_watcher::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::weight_watcher::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
